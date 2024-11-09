e6843ce1d4e1baf19bfa0ae262f6e62b75f50c2c
Quinton Tracey <qistoocool@gmail.com>
2024-11-09 13:02:18 -0500

    re up

--- src/main/java/codelook/example/introductiontojpa/AccessingDataJpaApplication.java
+++ src/main/java/codelook/example/introductiontojpa/AccessingDataJpaApplication.java
@@ -22,56 +22,56 @@ public class AccessingDataJpaApplication {
 
     @Bean
     public CommandLineRunner demo(BuddyInfoRepo repository, AddressBookRepo addressBookRepo) {
-        return (args) -> {
-            // save a few Buddies
-            BuddyInfo buddyInfo1 = new BuddyInfo("Jack", "4167777777");
-            BuddyInfo buddyInfo2 = new BuddyInfo("Jill", "4168888888");
-            BuddyInfo buddyInfo3 = new BuddyInfo("Hill", "4169999999");
-
-
-            AddressBook addressBook1 = new AddressBook();
-            AddressBook addressBook2 = new AddressBook();
-
-            addressBook1.addBuddy(buddyInfo1);
-            addressBook1.addBuddy(buddyInfo2);
-
-            addressBook2.addBuddy(buddyInfo3);
-
-            addressBookRepo.save(addressBook1);
-            addressBookRepo.save(addressBook2);
-
-
-            // fetch all Buddies
-            log.info("Buddies found with findAll():");
-            log.info("-------------------------------");
-            repository.findAll().forEach(buddy -> {
-                log.info(buddy.toString());
-            });
-            log.info("");
-
-            // fetch an individual Buddy by ID
-            BuddyInfo buddy = repository.findById(1L);
-            log.info("Customer found with findById(1L):");
-            log.info("--------------------------------");
-            log.info(buddy.toString());
-            log.info("");
-
-            // fetch customers by last name
-            log.info("Customer found with findByName('Jill'):");
-            log.info("--------------------------------------------");
-            repository.findByName("Jill").forEach(Jill -> {
-                log.info(Jill.toString());
-            });
-            log.info("");
-
-            // fetch all AddressBooks
-
-            log.info("AddressBooks found with findAll():");
-            log.info("-------------------------------");
-            addressBookRepo.findAll().forEach(addressBook -> {
-                log.info(addressBook.toString());
-            });
-            log.info("");
+//        return (args) -> {
+//            // save a few Buddies
+//            BuddyInfo buddyInfo1 = new BuddyInfo("Jack", "4167777777");
+//            BuddyInfo buddyInfo2 = new BuddyInfo("Jill", "4168888888");
+//            BuddyInfo buddyInfo3 = new BuddyInfo("Hill", "4169999999");
+//
+//
+//            AddressBook addressBook1 = new AddressBook();
+//            AddressBook addressBook2 = new AddressBook();
+//
+//            addressBook1.addBuddy(buddyInfo1);
+//            addressBook1.addBuddy(buddyInfo2);
+//
+//            addressBook2.addBuddy(buddyInfo3);
+//
+//            addressBookRepo.save(addressBook1);
+//            addressBookRepo.save(addressBook2);
+//
+//
+////            // fetch all Buddies
+////            log.info("Buddies found with findAll():");
+////            log.info("-------------------------------");
+////            repository.findAll().forEach(buddy -> {
+////                log.info(buddy.toString());
+////            });
+////            log.info("");
+////
+////            // fetch an individual Buddy by ID
+////            BuddyInfo buddy = repository.findById(1L);
+////            log.info("Customer found with findById(1L):");
+////            log.info("--------------------------------");
+////            log.info(buddy.toString());
+////            log.info("");
+////
+////            // fetch customers by last name
+////            log.info("Customer found with findByName('Jill'):");
+////            log.info("--------------------------------------------");
+////            repository.findByName("Jill").forEach(Jill -> {
+////                log.info(Jill.toString());
+////            });
+////            log.info("");
+////
+////            // fetch all AddressBooks
+////
+////            log.info("AddressBooks found with findAll():");
+////            log.info("-------------------------------");
+////            addressBookRepo.findAll().forEach(addressBook -> {
+////                log.info(addressBook.toString());
+////            });
+////            log.info("");
         };
     }
 
